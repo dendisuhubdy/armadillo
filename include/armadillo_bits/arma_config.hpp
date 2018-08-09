588650eaa807db1d6845e76a6be78234d45a51dd
conrad <conrad@localhost.localdomain>
2018-08-09 19:04:00 +1000

    consistency

--- include/armadillo_bits/arma_config.hpp
+++ include/armadillo_bits/arma_config.hpp
@@ -29,9 +29,9 @@ struct arma_config
   
   
   #if defined(ARMA_OPENMP_THRESHOLD)
-    static const uword mp_threshold = (sword(ARMA_OPENMP_THRESHOLD) > 0) ? uword(ARMA_OPENMP_THRESHOLD) : 320;
+    static const uword mp_threshold = (sword(ARMA_OPENMP_THRESHOLD) > 0) ? uword(ARMA_OPENMP_THRESHOLD) : 240;
   #else
-    static const uword mp_threshold = 320;
+    static const uword mp_threshold = 240;
   #endif
   
   
