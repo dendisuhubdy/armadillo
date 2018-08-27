da3f235197c7287e6557d48160975d40d558261a
conrad <conrad@localhost.localdomain>
2018-08-27 13:41:33 +1000

    mark as unstable

--- include/armadillo_bits/arma_version.hpp
+++ include/armadillo_bits/arma_version.hpp
@@ -20,9 +20,9 @@
 
 
 #define ARMA_VERSION_MAJOR 9
-#define ARMA_VERSION_MINOR 100
-#define ARMA_VERSION_PATCH 5
-#define ARMA_VERSION_NAME  "Armatus Ad Infinitum"
+#define ARMA_VERSION_MINOR 199
+#define ARMA_VERSION_PATCH 0
+#define ARMA_VERSION_NAME  "unstable"
 
 
 
