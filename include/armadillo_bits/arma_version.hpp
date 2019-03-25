a8f657d9d6a41997aac685338ffc0ab23a33b217
conrad <conrad@localhost.localdomain>
2019-03-25 23:42:37 +1000

    adjust version

--- include/armadillo_bits/arma_version.hpp
+++ include/armadillo_bits/arma_version.hpp
@@ -19,8 +19,8 @@
 
 
 
-#define ARMA_VERSION_MAJOR 10
-#define ARMA_VERSION_MINOR 0
+#define ARMA_VERSION_MAJOR 9
+#define ARMA_VERSION_MINOR 400
 #define ARMA_VERSION_PATCH 0
 #define ARMA_VERSION_NAME  "unstable"
 
