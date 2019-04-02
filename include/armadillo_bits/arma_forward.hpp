65b1a87e3b26daaf31952b6fbd2b1c78589ee4f4
conrad <conrad@localhost.localdomain>
2019-04-02 12:08:43 +1000

    use mtSpGlue framework for spglue_plus_mixed

--- include/armadillo_bits/arma_forward.hpp
+++ include/armadillo_bits/arma_forward.hpp
@@ -319,6 +319,9 @@ class spglue_minus;
 class spglue_schur;
 class spglue_times;
 
+class spglue_plus_mixed;
+class spglue_minus_mixed;  // TODO
+class spglue_schur_mixed;  // TODO
 class spglue_times_mixed;
 
 
