07833891e219d40b89cf085a16a8f108ae5e9198
conrad <conrad@localhost.localdomain>
2019-04-13 00:27:09 +1000

    simplifcation + optimisation

--- include/armadillo_bits/op_cov_meat.hpp
+++ include/armadillo_bits/op_cov_meat.hpp
@@ -27,67 +27,39 @@ op_cov::direct_cov(Mat<eT>& out, const Mat<eT>& A, const uword norm_type)
   {
   arma_extra_debug_sigprint();
   
-  if(A.is_vec())
-    {
-    if(A.n_rows == 1)
-      {
-      out = var(trans(A), norm_type);      
-      }
-    else
-      {
-      out = var(A, norm_type);
-      }
-    }
-  else
-    {
-    const uword N = A.n_rows;
-    const eT norm_val = (norm_type == 0) ? ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
-
-    const Row<eT> acc = sum(A);
-
-    out = trans(A) * A;
-    out -= (trans(acc) * acc)/eT(N);
-    out /= norm_val;
-    }
+  const Mat<eT> B( const_cast<eT*>(A.memptr()), A.n_cols, A.n_rows, false, true);
+  
+  const Mat<eT>& C = (A.n_rows == 1) ? B : A;
+  
+  const uword N        = C.n_rows;
+  const eT    norm_val = (norm_type == 0) ? ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
+  
+  const Mat<eT> tmp = C.each_row() - mean(C,0);
+  
+  out = tmp.t() * tmp;
+  out /= norm_val;
   }
 
 
 
-template<typename T>
+template<typename eT>
 inline
 void
-op_cov::direct_cov(Mat< std::complex<T> >& out, const Mat< std::complex<T> >& A, const uword norm_type)
+op_cov::direct_cov_htrans(Mat<eT>& out, const Mat<eT>& A, const uword norm_type)
   {
   arma_extra_debug_sigprint();
   
-  typedef typename std::complex<T> eT;
+  const Mat<eT> B( const_cast<eT*>(A.memptr()), A.n_cols, A.n_rows, false, true);
   
-  if(A.is_vec())
-    {
-    if(A.n_rows == 1)
-      {
-      const Mat<T> tmp_mat = var(trans(A), norm_type);
-      out.set_size(1,1);
-      out[0] = tmp_mat[0];
-      }
-    else
-      {
-      const Mat<T> tmp_mat = var(A, norm_type);
-      out.set_size(1,1);
-      out[0] = tmp_mat[0];
-      }
-    }
-  else
-    {
-    const uword N = A.n_rows;
-    const eT norm_val = (norm_type == 0) ? ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
-    
-    const Row<eT> acc = sum(A);
-    
-    out = trans(A) * A;               // out = strans(conj(A)) * A;
-    out -= (trans(acc) * acc)/eT(N);  // out -= (strans(conj(acc)) * acc)/eT(N);
-    out /= norm_val;
-    }
+  const Mat<eT>& C = (A.n_cols == 1) ? B : A;
+  
+  const uword N        = C.n_cols;
+  const eT    norm_val = (norm_type == 0) ? ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
+  
+  const Mat<eT> tmp = C.each_col() - mean(C,1);
+  
+  out = tmp * tmp.t();
+  out /= norm_val;
   }
 
 
@@ -101,12 +73,62 @@ op_cov::apply(Mat<typename T1::elem_type>& out, const Op<T1,op_cov>& in)
   
   typedef typename T1::elem_type eT;
   
-  const unwrap_check<T1> tmp(in.m, out);
-  const Mat<eT>& A     = tmp.M;
+  const uword norm_type = in.aux_uword_a;
+  
+  const quasi_unwrap<T1> U(in.m);
+  const Mat<eT>& A     = U.M;
+  
+  if(U.is_alias(out))
+    {
+    Mat<eT> tmp;
+    
+    op_cov::direct_cov(tmp, A, norm_type);
+    
+    out.steal_mem(tmp);
+    }
+  else
+    {
+    op_cov::direct_cov(out, A, norm_type);
+    }
+  }
+
+
+
+template<typename T1>
+inline
+void
+op_cov::apply(Mat<typename T1::elem_type>& out, const Op< Op<T1,op_htrans>, op_cov>& in)
+  {
+  arma_extra_debug_sigprint();
+  
+  typedef typename T1::elem_type eT;
   
   const uword norm_type = in.aux_uword_a;
   
-  op_cov::direct_cov(out, A, norm_type);
+  if(is_cx<eT>::no)
+    {
+    const quasi_unwrap<T1> U(in.m.m);
+    const Mat<eT>& A     = U.M;
+    
+    if(U.is_alias(out))
+      {
+      Mat<eT> tmp;
+      
+      op_cov::direct_cov_htrans(tmp, A, norm_type);
+      
+      out.steal_mem(tmp);
+      }
+    else
+      {
+      op_cov::direct_cov_htrans(out, A, norm_type);
+      }
+    }
+  else
+    {
+    const Mat<eT> tmp = in.m;  // force the evaluation of Op<T1,op_htrans>
+    
+    op_cov::direct_cov(out, tmp, norm_type);
+    }
   }
 
 
