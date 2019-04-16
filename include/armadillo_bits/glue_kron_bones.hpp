72db7e4c12fbc0341820f1c623cee06a45b17fcc
conrad <conrad@localhost.localdomain>
2019-04-16 19:43:34 +1000

    specialised traits

--- include/armadillo_bits/glue_kron_bones.hpp
+++ include/armadillo_bits/glue_kron_bones.hpp
@@ -21,10 +21,17 @@
 
 
 class glue_kron
-  : public glue_default_traits
   {
   public:
-
+  
+  template<typename T1, typename T2>
+  struct traits
+    {
+    static const bool is_row  = (T1::is_row && T2::is_row);
+    static const bool is_col  = (T1::is_col && T2::is_col);
+    static const bool is_xvec = false;
+    };
+  
   template<typename eT> inline static void direct_kron(Mat<eT>&                out, const Mat<eT>&                A, const Mat<eT>&                B);
   template<typename T>  inline static void direct_kron(Mat< std::complex<T> >& out, const Mat< std::complex<T> >& A, const Mat<T>&                 B);
   template<typename T>  inline static void direct_kron(Mat< std::complex<T> >& out, const Mat<T>&                 A, const Mat< std::complex<T> >& B);
@@ -35,4 +42,3 @@ class glue_kron
 
 
 //! @}
-
