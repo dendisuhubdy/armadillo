cd812172f7e810e0bfbccd8ade7ef2e54947570e
conrad <conrad@localhost.localdomain>
2019-04-17 17:53:27 +1000

    trait specialisations

--- include/armadillo_bits/op_fft_bones.hpp
+++ include/armadillo_bits/op_fft_bones.hpp
@@ -21,7 +21,7 @@
 
 
 class op_fft_real
-  : public traits_op_default  // TODO
+  : public traits_op_passthru
   {
   public:
   
@@ -32,7 +32,7 @@ class op_fft_real
 
 
 class op_fft_cx
-  : public traits_op_default  // TODO
+  : public traits_op_passthru
   {
   public:
   
@@ -50,7 +50,7 @@ class op_fft_cx
 
 
 class op_ifft_cx
-  : public traits_op_default  // TODO
+  : public traits_op_passthru
   {
   public:
   
