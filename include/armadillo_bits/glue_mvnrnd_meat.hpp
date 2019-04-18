cb6e5b8e39510ecbd7bd515375ee2cdbf588c78a
conrad <conrad@localhost.localdomain>
2019-04-18 12:02:22 +1000

    cleanup

--- include/armadillo_bits/glue_mvnrnd_meat.hpp
+++ include/armadillo_bits/glue_mvnrnd_meat.hpp
@@ -81,10 +81,18 @@ glue_mvnrnd::apply_direct(Mat<typename T1::elem_type>& out, const Base<typename 
     return true;
     }
   
-  bool status = false;
+  // if(auxlib::rudimentary_sym_check(UC.M) == false)
+  //   {
+  //   arma_debug_warn("mvnrnd(): given matrix is not symmetric");
+  //   return false;
+  //   }
   
-  // if(auxlib::rudimentary_sym_check(UC.M) == false)  { arma_debug_warn("mvnrnd(): given matrix is not symmetric"); return false; }
-  if((arma_config::debug) && (auxlib::rudimentary_sym_check(UC.M) == false))  { arma_debug_warn("mvnrnd(): given matrix is not symmetric"); }
+  if((arma_config::debug) && (auxlib::rudimentary_sym_check(UC.M) == false))
+    {
+    arma_debug_warn("mvnrnd(): given matrix is not symmetric");
+    }
+  
+  bool status = false;
   
   if(UM.is_alias(out) || UC.is_alias(out))
     {
